8f2728fb06e16a3a4df4dcbbb24ac1d0d47061ed
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-11-15 11:25:34 +0000

    Creamos TaskDao.kt

--- app/src/main/java/org/iesharia/fabioroom/Task.kt
+++ app/src/main/java/org/iesharia/fabioroom/Task.kt
@@ -1,4 +1,12 @@
 package org.iesharia.fabioroom
 
-class Task {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val name: String,
+    val isCompleted: Boolean = false
+)
