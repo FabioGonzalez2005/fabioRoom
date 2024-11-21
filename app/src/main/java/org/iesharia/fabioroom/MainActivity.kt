a94463298e3fe99bf1fe406e15d1791a1c2b7ccb
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-11-21 11:32:29 +0000

    Añadimos valores por defecto

--- app/src/main/java/org/iesharia/fabioroom/MainActivity.kt
+++ app/src/main/java/org/iesharia/fabioroom/MainActivity.kt
@@ -3,16 +3,16 @@ package org.iesharia.fabioroom
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 import org.iesharia.fabioroom.data.AppDatabase
-import org.iesharia.fabioroom.view.TaskApp
 
 class MainActivity : ComponentActivity() {
-
     private lateinit var database: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         // Inicializar la base de datos
         database = AppDatabase.getDatabase(this)
 
@@ -21,5 +21,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
