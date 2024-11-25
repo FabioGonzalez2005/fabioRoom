eb9d5ce9e5d5e4a5ea1f8b73e467de6e86cd2db9
FabioGonzalez2005 <fabiogonzalezw@gmail.com>
2024-11-25 12:38:52 +0000

    Agregamos fuente bold a los títulos

--- app/src/main/java/org/iesharia/fabioroom/view/TaskApp.kt
+++ app/src/main/java/org/iesharia/fabioroom/view/TaskApp.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -51,7 +52,7 @@ fun TaskApp(database: AppDatabase) {
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = "Tipos de tarea", style = MaterialTheme.typography.h5, modifier = Modifier.padding(bottom = 8.dp))
+        Text(text = "Tipos de tarea", style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold), modifier = Modifier.padding(bottom = 8.dp))
         OutlinedTextField(
             value = if (editingTipoTarea != null) editingTipoTareaName else newTypeTaskName,
             onValueChange = {
@@ -118,7 +119,7 @@ fun TaskApp(database: AppDatabase) {
             }
         }
         Spacer(modifier = Modifier.height(16.dp))
-        Text(text = "Tareas", style = MaterialTheme.typography.h5, modifier = Modifier.padding(vertical = 8.dp))
+        Text(text = "Tareas", style = MaterialTheme.typography.h5.copy(fontWeight = FontWeight.Bold), modifier = Modifier.padding(vertical = 8.dp))
         OutlinedTextField(
             value = if (editingTask != null) editingTaskName else newTaskName,
             onValueChange = {
